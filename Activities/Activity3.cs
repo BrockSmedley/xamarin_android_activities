4e9cb9d81dc81d2b0accdb887ec963e7be3f1318
Brock Smedley <brocksmedley@gmail.com>
2016-07-11 19:51:53 -0700

    added some comments for you <3

--- Activities/Activity3.cs
+++ Activities/Activity3.cs
@@ -15,17 +15,23 @@ namespace Activities {
 	public class Activity3 : Activity {
 		protected override void OnCreate(Bundle savedInstanceState) {
 			base.OnCreate(savedInstanceState);
+			//setting our special layout as this activity's content view
 			SetContentView(Resource.Layout.GetInfo);
 
 			Button sendButton = FindViewById<Button>(Resource.Id.buttonSend);
 			EditText textBox = FindViewById<EditText>(Resource.Id.editText1);
 			
 			sendButton.Click += delegate {
+				//create a new intent to send back with the result
 				Intent intent = new Intent(this, typeof(MainActivity));
+				//get value from textbox
 				string value = textBox.Text;
+				//send this value with the intent
 				intent.PutExtra("result", value);
+				//set result code; lets calling activity know whether the process worked
 				SetResult(Result.Ok, intent);
-				Finish(); //close out this activity; send result
+				//close out this activity; send result
+				Finish(); 
 			};
 		}
 	}
--- Activities/MainActivity.cs
+++ Activities/MainActivity.cs
@@ -19,23 +19,26 @@ namespace Activities {
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button button = FindViewById<Button>(Resource.Id.MyButton);
-
 			button.Click += delegate {
+				//start a plain activity
 				StartActivity(typeof(Activity2));
 			};
 
+			
 			Button button2 = FindViewById<Button>(Resource.Id.button2);
-
 			button2.Click += delegate {
+				//start a new activity and expect a result intent
 				var resultIntent = new Intent(this, typeof(Activity3));
 				StartActivityForResult(resultIntent, 0); // 0 is an arbitrary code... this should be used more effectively in real apps
 			};
 		}
 
+		//handles results from activities that are called for a result
 		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data) {
 			base.OnActivityResult(requestCode, resultCode, data);
 
 			if (resultCode == Result.Ok) {
+				//get the info from the intent and slap it on our TextView
 				var resultText = FindViewById<TextView>(Resource.Id.textViewResult);
 				resultText.SetText(data.GetStringExtra("result"), TextView.BufferType.Normal);
 			}
